b819f38f30c5eb881028083bf6cf4de0ce79cf02
senyu <jasipoo@proton.me>
2026-08-02 16:51:28 -0500

    added health endpoint

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,4 +1,25 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func healthzHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	// renders a blank screen -> expected behavior!
+}
+
 func main() {
+	r := chi.NewRouter()
+
+	r.Get("/healthz", healthzHandler)
+
+	if ok := http.ListenAndServe(":3000", r); ok != nil {
+		fmt.Fprintln(os.Stderr, "Error starting server: %v", ok.Error())
+		os.Exit(-1)
+	}
 }
