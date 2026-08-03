2ee81075c17f4e5f6318bdbd02f0cbe15e17c34f
senyu <jasipoo@proton.me>
2026-08-03 17:25:14 -0500

    stub for prometheus fuck my life

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 	"os"
 
 	"github.com/go-chi/chi/v5"
+
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func healthzHandler(w http.ResponseWriter, r *http.Request) {
@@ -13,13 +14,21 @@ func healthzHandler(w http.ResponseWriter, r *http.Request) {
 	// renders a blank screen -> expected behavior!
 }
 
+func metricsMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	})
+}
+
 func main() {
 	r := chi.NewRouter()
 
+	// chi middleware
+	r.Use(metricsMiddleware)
+
 	r.Get("/healthz", healthzHandler)
+	r.Handle("/metrics", promhttp.Handler())
 
 	if ok := http.ListenAndServe(":3000", r); ok != nil {
-		fmt.Fprintln(os.Stderr, "Error starting server: %v", ok.Error())
 		os.Exit(-1)
 	}
 }
